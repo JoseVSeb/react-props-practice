0c1a6c8d2efacc130997cc0b11570d25f6d6faa4
Jose V Sebastian <josevseb99@gmail.com>
2021-10-06 22:48:28 +0530

    complete challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import contacts from "../contacts";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img src={props.imgURL} alt="avatar_img" className="circle-img" />
+      </div>
+      <div className="bottom">
+        <p className="info">{props.phone}</p>
+        <p className="info">{props.email}</p>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      {contacts.map(Card)}
     </div>
   );
 }
